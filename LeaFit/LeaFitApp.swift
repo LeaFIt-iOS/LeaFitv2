f5fddded99314db05093112083d44e093ee5f878
Koredeee <muhammad.fawwaz003@binus.ac.id>
2025-06-09 12:06:40 +0700

    Initial Commit

---
+++ LeaFit/LeaFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaFitApp.swift
+//  LeaFit
+//
+//  Created by FWZ on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LeaFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
